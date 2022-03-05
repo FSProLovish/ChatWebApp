82743ed14d3abcd95ca067107f4ef219d91205c2
FSProLovish <lavishgarg006@gmail.com>
2022-03-05 10:19:28 +0530

    05-03-2022

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ app.use(express.urlencoded());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", function (req, res) {
+app.get("/sign-in", function (req, res) {
   return res.render("signIn");
+});
+
+app.get("/sign-up", function (req, res) {
+  return res.render("signUp");
 });
 
 app.listen(port, function (err) {
   if (err) {
     console.log("error in running the server", err);
   }
-  console.log("Yup!My express server is running on the port:", port);
+  console.log("My express server is running on the port:", port);
 });
