f0077bf9ea8af500b0ed526b02c96467b80830f6
FSProLovish <lavishgarg006@gmail.com>
2022-03-05 12:37:30 +0530

    05-03-2022

--- index.js
+++ index.js
@@ -6,12 +6,43 @@ const app = express();
 const db = require("./config/mongoose");
 const User = require("./models/user");
 
-app.use(express.urlencoded());
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
 
+app.use(express.static("assets"));
+
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+
+// Socket for Chat
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+// To Start Chat Server
+io.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    socket.broadcast.emit("message", data);
+  });
+});
+
+// Home page
+app.get("/", function (req, res) {
+  return res.render("home");
+});
+
+// Sign In page
 app.get("/sign-in", function (req, res) {
+  return res.render("signIn");
+});
+
+// Sign Up page
+app.get("/sign-up", function (req, res) {
+  return res.render("signUp");
+});
+
+// User's Sign In page
+app.get("/users/sign-in", function (req, res) {
   User.findOne(
     {
       email: req.body.email,
@@ -25,7 +56,8 @@ app.get("/sign-in", function (req, res) {
   );
 });
 
-app.post("/sign-up", function (req, res) {
+// User's Sign Up page
+app.post("/users/sign-up", function (req, res) {
   User.findOne(
     {
       email: req.body.email,
@@ -57,7 +89,12 @@ app.post("/sign-up", function (req, res) {
   );
 });
 
-app.listen(port, function (err) {
+// Chatting Window
+app.get("/users/chat", function (req, res) {
+  return res.render("chat_box");
+});
+
+server.listen(port, function (err) {
   if (err) {
     console.log("error in running the server", err);
   }
