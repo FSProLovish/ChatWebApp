44c5bd8df4b6291a654b67f14ed2bccbc33fcd9c
FSProLovish <lavishgarg006@gmail.com>
2022-03-05 10:35:25 +0530

    05-03-2022

--- index.js
+++ index.js
@@ -3,17 +3,58 @@ const port = 8000;
 const path = require("path");
 const app = express();
 
+const db = require("./config/mongoose");
+const User = require("./models/user");
+
 app.use(express.urlencoded());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/sign-in", function (req, res) {
-  return res.render("signIn");
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    function (err, user) {
+      if (err) {
+        console.log("error in finding the user");
+        return;
+      }
+    }
+  );
 });
 
-app.get("/sign-up", function (req, res) {
-  return res.render("signUp");
+app.post("/sign-up", function (req, res) {
+  User.findOne(
+    {
+      email: req.body.email,
+    },
+    function (err, user) {
+      if (err) {
+        console.log("error in finding the user");
+        return;
+      }
+      if (!user) {
+        User.create(
+          {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+          },
+          function (err, newUser) {
+            if (err) {
+              console.log("error in creating a user");
+              return;
+            }
+            return res.redirect("/sign-in");
+          }
+        );
+      } else {
+        res.redirect("back");
+      }
+    }
+  );
 });
 
 app.listen(port, function (err) {
